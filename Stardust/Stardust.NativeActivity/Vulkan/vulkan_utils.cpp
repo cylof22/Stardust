372c2595c1d894807db2853a59626ac5b9f47563
cylof22 <cylof22@gmail.com>
2017-07-08 11:21:59 +0800

    Fix depth_stencile access flag error

--- Stardust/Stardust.NativeActivity/Vulkan/vulkan_utils.cpp
+++ Stardust/Stardust.NativeActivity/Vulkan/vulkan_utils.cpp
@@ -282,7 +282,7 @@ void setImageLayout(VkCommandBuffer cmdbuffer, VkImage image, VkImageLayout oldI
 	case VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL:
 		// Image layout will be used as a depth/stencil attachment
 		// Make sure any writes to depth/stencil buffer have been finished
-		imageMemoryBarrier.dstAccessMask = imageMemoryBarrier.dstAccessMask | VK_ACCESS_DEPTH_STENCIL_ATTACHMENT_WRITE_BIT;
+		imageMemoryBarrier.dstAccessMask = VK_ACCESS_DEPTH_STENCIL_ATTACHMENT_WRITE_BIT;
 		break;
 
 	case VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL:
